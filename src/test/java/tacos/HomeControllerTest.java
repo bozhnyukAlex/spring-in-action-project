3417aca1d477780e97f9d81d53282e333d300074
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2022-04-08 18:11:39 +0300

    [2.4] Configure view

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
     @Autowired
