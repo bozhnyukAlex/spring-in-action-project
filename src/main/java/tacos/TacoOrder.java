2af9ec20123907fe770c009adb7c9459decdb141
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2022-04-09 14:31:25 +0300

    [3.1.1] Adapting the domain for persistence

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -14,6 +14,8 @@ import java.util.List;
 @Data
 public class TacoOrder implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private Long id;
 
     private Date createdAt = new Date();
