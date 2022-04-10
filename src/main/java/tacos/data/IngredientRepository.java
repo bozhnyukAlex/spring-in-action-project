9d439043e25043ce74d375349432cb0aa3c32eb9
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2022-04-10 19:11:54 +0300

    [3.2.2] Defining repository interfaces

--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -1,13 +1,5 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
 import tacos.Ingredient;
-
-import java.util.Optional;
-
-public interface IngredientRepository {
-    Iterable<Ingredient> findAll();
-
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
-}
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {}
