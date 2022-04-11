f03ab2e5d24bb6f3250ad6d38e6351eecde13697
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2022-04-11 19:21:28 +0300

    [3.3.4] Customizing repositories

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,7 +1,19 @@
 package tacos.data;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tacos.TacoOrder;
 
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {}
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    @Query("Order o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+}
 
