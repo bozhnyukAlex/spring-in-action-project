5ace45ca655eb804daeb7fd479a51dd59a853f4e
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2022-04-11 18:07:06 +0300

    [3.3.2] Annotating the domain as entities

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -4,14 +4,17 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.relational.core.mapping.Table;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
-@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 public class Ingredient {
 
+    @Id
     private final String id;
     private final String name;
     private final Type type;
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,9 +1,8 @@
 package tacos;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -11,10 +10,11 @@ import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt = new Date();
@@ -23,7 +23,11 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @NotNull
     @Size(min=1, message="You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients = new ArrayList<>();
+    @ManyToMany()
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 }
